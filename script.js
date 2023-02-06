284eb63303200df74708ff7e27c5bdfaae302c47
Isengrine <isengrine@gmail.com>
2023-02-06 10:33:38 -0600

    Add button script variables

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function addInput() {
+    inputScreen.innerText = inputValue;
+}
+
 function add() {
 
 }
@@ -21,3 +25,28 @@ function operate() {
 function clear() {
     
 }
+
+const inputScreen = document.getElementById("input");
+const outputScreen = document.getElementById("output");
+
+let inputValue = 0;
+let outputValue = 0;
+
+const add = document.getElementById("+");
+const substract = document.getElementById("-");
+const multiply = document.getElementById("x");
+const divide = document.getElementById("/");
+const equals = document.getElementById("=");
+const del = document.getElementById("del");
+const clear = document.getElementById("C");
+
+const zero = document.getElementById("0");
+const one = document.getElementById("1");
+const two = document.getElementById("2");
+const three = document.getElementById("3");
+const four = document.getElementById("4");
+const five = document.getElementById("5");
+const six = document.getElementById("6");
+const seven = document.getElementById("7");
+const eight = document.getElementById("8");
+const nine = document.getElementById("9");
