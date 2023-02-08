266093674c9c30a9c7c1c41fd1218043824fedbc
Isengrine <isengrine@gmail.com>
2023-02-08 09:30:32 -0600

    Add firstNum and secondNum vars and input

--- script.js
+++ script.js
@@ -29,14 +29,28 @@ function clear() {
 const numberBtns = document.querySelectorAll(".number");
 const operatorBtns = document.querySelectorAll(".operator");
 
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+
 numberBtns.forEach(numberBtn => {
     numberBtn.addEventListener("click", () => {
-        console.log(numberBtn.textContent);
+        if (operator == "") {
+            firstNum += numberBtn.textContent;
+            console.log(firstNum);
+        }
+
+        else {
+            secondNum += numberBtn.textContent;
+            console.log(secondNum);
+        }
     });
 });
 
 operatorBtns.forEach(operatorBtn => {
     operatorBtn.addEventListener("click", () => {
-        console.log(operatorBtn.textContent);
+        operator = "";
+        operator = operatorBtn.textContent;
+        console.log(operator);
     });
 });
