55969ea1ace56cf04a8395a21984fb5239e3792a
Isengrine <isengrine@gmail.com>
2023-02-08 11:05:02 -0600

    Add delete and clear functions

--- script.js
+++ script.js
@@ -27,29 +27,47 @@ function operate() {
             return multiply(firstNum, secondNum);
         case "/":
             return divide(firstNum, secondNum);
+        default:
+            break;
     }
 }
 
 function del() {
-
+    firstNum = firstNum.slice(0, -1);
+    input();
 }
 
 function clear() {
-
+    firstNum = "";
+    secondNum = "";
+    operator = "";
+    input();
 }
 
 function input() {
-    mainScreen.innerText = firstNum;
+    if (firstNum == "") {
+        mainScreen.innerText = 0;
+    }
+    else {
+        mainScreen.innerText = firstNum;
+    }
 }
 
 const numberBtns = document.querySelectorAll(".number");
 const operatorBtns = document.querySelectorAll(".operator");
+const equalsBtn = document.getElementById("=");
+const delBtn = document.getElementById("del");
+const clearBtn = document.getElementById("C");
 const mainScreen = document.getElementById("mainScreen");
 const auxScreen = document.getElementById("auxScreen");
 
 let firstNum = "";
 let secondNum = "";
 let operator = "";
+
+delBtn.addEventListener("click", del);
+
+clearBtn.addEventListener("click", clear);
 
 numberBtns.forEach(numberBtn => {
     numberBtn.addEventListener("click", () => {
